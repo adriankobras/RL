c11b2daf21fe9df641d4997918493192e913b037
Adrian Kobras <adrian.kobras@protonmail.com>
2024-04-25 21:11:56 +0200

    Added to stable diffusion

--- stable_diffusion.py
+++ stable_diffusion.py
@@ -17,4 +17,24 @@ ctk.set_appearance_mode("dark")
 prompt = ctk.CTkEntry(master=app, height=40, width=512, font=("Arial", 20), text_color="black", fg_color="white")
 prompt.place(x=10, y=10)
 
+lmain = tk.Label(height=512, width=512)
+lmain.place(x=10, y=110)
+
+modelid = "CompVis/stable-diffusion-v1-4"
+device = "mps"
+pipe = StableDiffusionPipeline(modelid, revision="fp16", torch_dtype=torch.float16, use_auth_token=True, auth_token=AuthToken())
+pipe.to(device)
+
+def generate():
+    with autocast(device):
+        image = pipe(prompt.get(), guidance_scale=8.5)["sample"][0]
+
+    img = ImageTk.PhotoImage(image)
+    img.save("generatedimage.png")
+    lmain.configure(image=img)
+
+trigger = ctk.CTkButton(master=app, height=40, width=120, font=("Arial", 20), text_color="white", fg_color="blue", command=generate)
+trigger.configure(text="Generate")
+trigger.place(x=206, y=60)
+
 app.mainloop()
